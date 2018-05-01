3852bec5a5c0283c3de5a7673c25187553a5d2c7
usoewin <insomniacui@hotmail.com>
2018-04-30 22:30:43 -0700

    cuda time added

--- go_Decision_cuda.cpp
+++ go_Decision_cuda.cpp
@@ -17,28 +17,40 @@ int main(int argc, char** argv)
 {
     int size = 19;
     GameBoard* board = new GameBoard;
-    //board_construct(board, size);
+
+    ofstream myfile ("time_cuda.txt");
+    if (myfile.is_open())
+    {
+        printf("file opened\n");
+    }
+    
     buildBoard(board,size);
-    int row, col, next_move;
+    int row, col, next_move,step;
+    step = 0;
+
     cin >> row;
     while (row != -1){
         cin >> col;
-        //board_addStone(board, row, col, 1);
+       step += 1;
         addStone(board, row, col, 1);
-        //next_move = board_monte_carlo(board, 2);
+
        	std::clock_t start;
        	double duration = 0;
         next_move = Monte_Carlo_Cuda(board, 3);
         duration = (std::clock() - start)/(double)CLOCKS_PER_SEC;
         printf("add white stone %d\n", next_move);
-        //while (board_addStone(board, next_move / size, next_move % size, -1) == 0){
+
         while(addStone(board, next_move/size, next_move%size, -1) ==0 ){
             next_move = rand() % (size * size);
         }
         printBoard(board);
         printf("Time is  %f\n", duration);
+        printf("Step is  %d\n", step);
+        myfile << duration << " "<<step<<endl; 
         cin >> row;
     }
+    
+    myfile.close();
     delete board;
     return 0;
 }
