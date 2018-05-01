05a0dcde257ebdf39434c8672186c7075d3e6f98
usoewin <insomniacui@hotmail.com>
2018-04-30 22:25:11 -0700

    serial time done

--- go_Decision.cpp
+++ go_Decision.cpp
@@ -16,29 +16,38 @@ int Monte_Carlo_Cuda(GameBoard* this_board, int n);
 int main(int argc, char** argv)
 {
     int size = 19;
+   	ofstream myfile ("time_serial.txt");
+   	if (myfile.is_open())
+   	{
+   		printf("file opened\n");
+   	}
     GameBoard* board = new GameBoard;
-    //board_construct(board, size);
     buildBoard(board,size);
-    int row, col, next_move;
+    int row, col, next_move,step;
+    step = 0;
     cin >> row;
     while (row != -1){
         cin >> col;
-        //board_addStone(board, row, col, 1);
+        step+=1;
         addStone(board, row, col, 1);
-        //next_move = board_monte_carlo(board, 2);
+        
        	std::clock_t start;
        	double duration = 0;
         next_move = kernelMonteCarlo(board, 3);
         duration = (std::clock() - start)/(double)CLOCKS_PER_SEC;
         printf("add white stone %d\n", next_move);
-        //while (board_addStone(board, next_move / size, next_move % size, -1) == 0){
+
+        
         while(addStone(board, next_move/size, next_move%size, -1) ==0 ){
             next_move = rand() % (size * size);
         }
         printBoard(board);
         printf("Time is  %f\n", duration);
+        printf("Step is  %d\n", step);
+        myfile << duration << " "<<step<<endl; 
         cin >> row;
     }
+    myfile.close();
     delete board;
     return 0;
 }
