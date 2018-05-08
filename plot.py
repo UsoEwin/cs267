368873e28516d2d53abff6aa9beb8b229c781db6
usoewin <insomniacui@hotmail.com>
2018-05-08 12:55:10 -0700

    final updata

--- plot.py
+++ plot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Kernel', 'Memcpy', 'Synch'
-sizes = [4, 7, 89]
-explode = (0.2, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+labels = 'Memcpy', 'Kernel', 'Synch', 'Cleanup and other'
+sizes = [16.4, 0.4, 28.6, 54.6]
+explode = (0, 0, 0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
